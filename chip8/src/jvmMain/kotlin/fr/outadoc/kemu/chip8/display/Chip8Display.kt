55f9ec6a49c260695a2e548cdbe7056b5dc51dfc
Baptiste Candellier <outadoc@gmail.com>
2020-11-21 12:21:33 +0100

    Implement framebuffer

--- chip8/src/jvmMain/kotlin/fr/outadoc/kemu/chip8/display/Chip8Display.kt
+++ chip8/src/jvmMain/kotlin/fr/outadoc/kemu/chip8/display/Chip8Display.kt
@@ -1,8 +1,10 @@
 package fr.outadoc.kemu.chip8.display
 
+import fr.outadoc.kemu.b
 import fr.outadoc.kemu.chip8.Chip8Constants
 import fr.outadoc.kemu.display.Display
 import fr.outadoc.kemu.display.Point
+import fr.outadoc.kemu.shr
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
@@ -16,17 +18,34 @@ actual class Chip8Display : Display, JComponent() {
         size = Dimension(Chip8Constants.DISPLAY_HEIGHT, Chip8Constants.DISPLAY_WIDTH)
     }
 
-    private val frameBuffer = UByteArray(
-        (Chip8Constants.DISPLAY_WIDTH / 8) * (Chip8Constants.DISPLAY_HEIGHT / 8)
-    )
+    private val frameBufferWidth = Chip8Constants.DISPLAY_WIDTH / 8
+    private val frameBufferHeight = Chip8Constants.DISPLAY_HEIGHT / 8
+    private val frameBuffer = UByteArray(frameBufferWidth * frameBufferHeight)
 
     override fun paintComponent(g: Graphics?) {
-        super.paintComponent(g)
         g ?: return
+        frameBuffer.forEachIndexed { i, byte ->
+            // Byte i in buffer is at screen position (i mod width, i / width)
+            val startX = i.rem(frameBufferWidth)
+            val y = i / frameBufferWidth
+
+            // Display each bit of the byte
+            for (bit in 0 until 8) {
+                // get the nth bit of the byte
+                // e.g for bit = 2: byte & 0b00000100 >> 2
+                val pixel = (byte and ((1 shl bit).toUByte())) shr bit
+
+                // Draw foreground color if bit is set, background otherwise
+                g.color = if (pixel == 0x0.b) background else foreground
+                g.drawRect(startX + bit, y, 1, 1)
+            }
+        }
     }
 
     override fun clear() {
-        TODO("Not yet implemented")
+        (0..frameBuffer.size).forEach { i ->
+            frameBuffer[i] = 0x0.b
+        }
     }
 
     override fun displaySprite(position: Point<UByte>, sprite: UByteArray): Boolean {
