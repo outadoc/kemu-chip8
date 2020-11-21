97a3000db49c90319030c5eb060fbf48eebb1854
Baptiste Candellier <outadoc@gmail.com>
2020-11-21 14:49:32 +0100

    Change framebuffer, add checkerboard

--- chip8/src/jvmMain/kotlin/fr/outadoc/kemu/chip8/display/Chip8Display.kt
+++ chip8/src/jvmMain/kotlin/fr/outadoc/kemu/chip8/display/Chip8Display.kt
@@ -4,7 +4,6 @@ import fr.outadoc.kemu.b
 import fr.outadoc.kemu.chip8.Chip8Constants
 import fr.outadoc.kemu.display.Display
 import fr.outadoc.kemu.display.Point
-import fr.outadoc.kemu.shr
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
@@ -14,10 +13,10 @@ import javax.swing.JComponent
 
 actual class Chip8Display : Display, JComponent() {
 
-    private val scaleFactor = 100.0
+    private val scaleFactor = 20.0
 
-    private val frameBufferWidth = Chip8Constants.DISPLAY_WIDTH / 8
-    private val frameBufferHeight = Chip8Constants.DISPLAY_HEIGHT / 8
+    private val frameBufferWidth = Chip8Constants.DISPLAY_WIDTH
+    private val frameBufferHeight = Chip8Constants.DISPLAY_HEIGHT
     private val frameBuffer = UByteArray(frameBufferWidth * frameBufferHeight)
 
     private val scaleTransform = AffineTransform().apply {
@@ -31,25 +30,21 @@ actual class Chip8Display : Display, JComponent() {
             (Chip8Constants.DISPLAY_HEIGHT * scaleFactor).toInt(),
             (Chip8Constants.DISPLAY_WIDTH * scaleFactor).toInt()
         )
+
+        displayCheckerBoard()
     }
 
     override fun paintComponent(g: Graphics?) {
-        g ?: return
-        frameBuffer.forEachIndexed { i, byte ->
+        if (g !is Graphics2D) return
+
+        frameBuffer.forEachIndexed { i, pixel ->
             // Byte i in buffer is at screen position (i mod width, i / width)
-            val startX = i.rem(frameBufferWidth)
+            val x = i % frameBufferWidth
             val y = i / frameBufferWidth
 
-            // Display each bit of the byte
-            for (bit in 0 until 8) {
-                // get the nth bit of the byte
-                // e.g for bit = 2: byte & 0b00000100 >> 2
-                val pixel = (byte and ((1 shl bit).toUByte())) shr bit
-
-                // Draw foreground color if bit is set, background otherwise
-                g.color = if (pixel == 0x0.b) background else foreground
-                g.drawRect(startX + bit, y, 1, 1)
-            }
+            // Draw foreground color if bit is set, background otherwise
+            g.color = if (pixel == 0x0.b) background else foreground
+            g.drawRect(x, y, 1, 1)
         }
     }
 
@@ -60,8 +55,17 @@ actual class Chip8Display : Display, JComponent() {
     }
 
     override fun clear() {
-        (0..frameBuffer.size).forEach { i ->
+        frameBuffer.indices.forEach { i ->
             frameBuffer[i] = 0x0.b
+        }
+    }
+
+    private fun displayCheckerBoard() {
+        frameBuffer.indices.forEach { i ->
+            val x = i % frameBufferWidth
+            val y = i / frameBufferWidth
+            frameBuffer[i] =
+                if ((x % 2 == 0 && y % 2 == 1) || (x % 2 == 1 && y % 2 == 0)) 0x0.b else 0x1.b
         }
     }
 
