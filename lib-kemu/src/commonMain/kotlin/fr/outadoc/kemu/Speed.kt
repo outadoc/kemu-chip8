c1e654285eba806fdcc472763e235f4456aab6e9
Baptiste Candellier <outadoc@gmail.com>
2020-11-24 22:04:01 +0100

    Change delay by platform

--- lib-kemu/src/commonMain/kotlin/fr/outadoc/kemu/Speed.kt
+++ lib-kemu/src/commonMain/kotlin/fr/outadoc/kemu/Speed.kt
@@ -1,9 +1,11 @@
 package fr.outadoc.kemu
 
+expect val REALTIME_DELAY_MS: Long
+
 enum class Speed(val delay: Long) {
     SUPER_SLOW(200L),
     SLOW(100L),
     NORMAL(50L),
     FAST(10L),
-    REALTIME(0L)
+    REALTIME(REALTIME_DELAY_MS)
 }
---
+++ lib-kemu/src/jsMain/kotlin/fr/outadoc/kemu/Speed.kt
@@ -0,0 +1,3 @@
+package fr.outadoc.kemu
+
+actual val REALTIME_DELAY_MS = 1L
