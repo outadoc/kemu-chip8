c19e24861b509886c708397c90806d15e14a1b28
Baptiste Candellier <outadoc@gmail.com>
2020-11-30 20:35:15 +0100

    Create base Chip8DisplayView.swift

---
+++ app-chip8-ios/iOS/Chip8DisplayView.swift
@@ -0,0 +1,17 @@
+//
+//  Chip8DisplayView.swift
+//  app-chip8-ios (iOS)
+//
+//  Created by Baptiste on 30/11/2020.
+//
+
+import Foundation
+import UIKit
+import lib_kemu
+
+class Chip8DisplayView : UIView, FrameConsumer {
+
+    func displayFrame(frame: UByteArray2?) {
+        
+    }
+}
