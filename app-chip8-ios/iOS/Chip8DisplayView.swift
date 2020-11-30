0450a132f983eff796f9a49bb00fb33cb5c50c33
Baptiste Candellier <outadoc@gmail.com>
2020-11-30 22:38:46 +0100

    Implement iOS display view \o/

--- app-chip8-ios/iOS/Chip8DisplayView.swift
+++ app-chip8-ios/iOS/Chip8DisplayView.swift
@@ -37,16 +37,54 @@ class Chip8DisplayView : UIView, FrameConsumer {
         foreground = theme.foreground
         super.init(frame: frame)
     }
-
-    func displayFrame(frame: UByteArray2?) {
-        // TODO
-        setNeedsDisplay()
-    }
     
     override var intrinsicContentSize: CGSize {
         return CGSize(
             width: (Int(Chip8Constants().DISPLAY_WIDTH) * scaleFactor),
             height: (Int(Chip8Constants().DISPLAY_HEIGHT) * scaleFactor)
         )
+    }
+
+    func displayFrame(frame: UByteArray2?) {
+        currentFrame = frame
+        setNeedsDisplay()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let ctx = UIGraphicsGetCurrentContext()
+        if let frame = currentFrame {
+            var i = 0
+            while (frame.iterator().hasNext()) {
+                let pixel = frame.iterator().next() as! NSNumber
+
+                // Byte i in buffer is at screen position (i mod width, i / width)
+                let x = i % Int(Chip8Constants().DISPLAY_WIDTH)
+                let y = i / Int(Chip8Constants().DISPLAY_WIDTH)
+
+                // Draw foreground color if bit is set, background otherwise
+                let color = pixel == 0x0 ? background : foreground
+                ctx?.setFillColor(color.toCGColor())
+
+                ctx?.fill(
+                    CGRect(
+                        x: CGFloat(x * scaleFactor),
+                        y: CGFloat(y * scaleFactor),
+                        width: CGFloat(scaleFactor),
+                        height: CGFloat(scaleFactor)
+                    )
+                )
+                
+                i += 1
+            }
+        } else {
+            super.draw(rect)
+        }
+    }
+}
+
+extension Color {
+    
+    func toCGColor() -> CGColor {
+        return CGColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: 1.0)
     }
 }
