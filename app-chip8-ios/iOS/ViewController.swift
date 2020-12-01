0c63dc1a2f94aacdcc4686b884459c44a0e27b03
Baptiste Candellier <outadoc@gmail.com>
2020-12-01 18:40:55 +0100

    Add basic project files

--- app-chip8-ios/iOS/ViewController.swift
+++ app-chip8-ios/iOS/ViewController.swift
@@ -5,13 +5,15 @@
 //  Created by Baptiste on 01/12/2020.
 //
 
-import Foundation
 import UIKit
- 
+
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
+
+
 }
+
