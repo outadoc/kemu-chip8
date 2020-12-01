5dc68dd44713de9d35c12db1c439ad83fd901a89
Baptiste Candellier <outadoc@gmail.com>
2020-12-01 18:00:38 +0100

    Basic storyboard

---
+++ app-chip8-ios/iOS/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  app-chip8-ios (iOS)
+//
+//  Created by Baptiste on 01/12/2020.
+//
+
+import Foundation
---
+++ app-chip8-ios/iOS/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  app-chip8-ios (iOS)
+//
+//  Created by Baptiste on 01/12/2020.
+//
+
+import Foundation
+import UIKit
+ 
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
